2547ad72a82f101ddc06fca9752bc5a3c5fdd8a6
Vwore <vanshaj.wore@gmail.com>
2025-03-05 15:29:41 +0530

    all task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./Card/card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [country, setCountry] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://xcountries-backend.azurewebsites.net/all")
+      .then((data) => {
+        console.log(data);
+        setCountry(data.data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+  return <div className="App">
+    <div className="card-container">
+      {country.map((data)=>(<Card data={data}/>))}
     </div>
-  );
+   
+  </div>;
 }
 
 export default App;
---
+++ src/Card/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './card.css'
+
+function Card({data}){
+    return (
+        <div className="card">
+            <img src={data.flag} alt={data.attr} style={{width: '80%',height:'80%'}}/>
+            <h3>{data.name}</h3>
+        </div>
+    )
+}
+
+export default Card;
